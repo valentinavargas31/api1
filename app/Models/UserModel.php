048e1c2a7ef27f06f6822a4e03e792aa1af97bd5
valentinavargas31 <137255256+valentinavargas31@users.noreply.github.com>
2024-05-16 21:04:45 -0500

    nuevo cambio
    edición del archivo UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,22 +6,18 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
+    protected $DBGroup          ='default';
     protected $table            = 'users';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['emmail','password'];
 
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    protected array $casts = [];
-    protected array $castHandlers = [];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
