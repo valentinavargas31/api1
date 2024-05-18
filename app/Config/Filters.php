eadf69b63dccd5bfabc07ca99afba53db83316cd
valentinavargas31 <137255256+valentinavargas31@users.noreply.github.com>
2024-05-18 14:46:58 -0500

    nuevo cambio
    ajustar el achivo Filter.php.
    se crearon rutas de proyecto en routes.php.

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,18 +2,18 @@
 
 namespace Config;
 
-use CodeIgniter\Config\Filters as BaseFilters;
-use CodeIgniter\Filters\Cors;
+use CodeIgniter\Config\BaseConfig;
+//use CodeIgniter\Filters\Cors;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
-use CodeIgniter\Filters\ForceHTTPS;
+//use CodeIgniter\Filters\ForceHTTPS;
 use CodeIgniter\Filters\Honeypot;
 use CodeIgniter\Filters\InvalidChars;
-use CodeIgniter\Filters\PageCache;
-use CodeIgniter\Filters\PerformanceMetrics;
+//use CodeIgniter\Filters\PageCache;
+//use CodeIgniter\Filters\PerformanceMetrics;
 use CodeIgniter\Filters\SecureHeaders;
 
-class Filters extends BaseFilters
+class Filters extends BaseConfig
 {
     /**
      * Configures aliases for Filter classes to
@@ -30,10 +30,11 @@ class Filters extends BaseFilters
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
-        'cors'          => Cors::class,
-        'forcehttps'    => ForceHTTPS::class,
-        'pagecache'     => PageCache::class,
-        'performance'   => PerformanceMetrics::class,
+        'authFilter' => \App\Filters\AuthFilter::class,
+        //'cors'          => Cors::class,
+        //'forcehttps'    => ForceHTTPS::class,
+        //'pagecache'     => PageCache::class,
+        //'performance'   => PerformanceMetrics::class,
     ];
 
     /**
